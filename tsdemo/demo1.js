7103b843da2cc2a3e857c8c868da3ac15a7e79c5
liushuang <liushuang@laiye.com>
2023-12-16 21:44:02 +0800

    first commit

---
+++ tsdemo/demo1.js
@@ -0,0 +1,7 @@
+function addOne(n) {
+    return n + 1;
+}
+var a = addOne(1);
+var message = "hello";
+console.log(message);
+console.log(a);
---
+++ tsdemo/demo1.ts
@@ -0,0 +1,11 @@
+function addOne(n: number) {
+    return n + 1;
+}
+
+
+let a = addOne(1)
+
+var message:string = "hello"
+
+console.log(message)
+console.log(a)
